b30016044e52391b1de037b513abb94f9c3a955a
Katelyn Gigante <clockwork.singularity@gmail.com>
2023-12-28 15:06:14 +1100

    Fix error when there were less than 21 floors

--- worlds/ffxiitm/Rules.py
+++ worlds/ffxiitm/Rules.py
@@ -13,16 +13,19 @@ def has_item(state: CollectionState, player: int, item) -> bool:
 def set_rules(world: "FFXIITMWorld", player: int):
     # Win condition.
     world.multiworld.completion_condition[player] = lambda state: state.has_all({"Victory"}, player)
+    max_floor = world.get_setting("trial_victory")
 
     def get_entrance(entrance: str):
         return world.multiworld.get_entrance(entrance, world.player)
 
-    set_rule( # We want second job early
-        get_entrance("11-20"),
-        lambda state: state.has("Second Job", world.player)
-    )
-    for floor in range(2, world.get_setting("trial_victory") // 10):
+    if max_floor > 10:
         set_rule(
-            get_entrance(str(floor * 10 + 1) + "-" + str(floor * 10 + 10)),
-            lambda state: state.count_group("Equipment", world.player) + state.count_group("Magick", world.player) + state.count_group("Technick", world.player) + state.count_group("Mist", world.player) >= floor * 4
+            get_entrance("11-20"),
+            lambda state: state.has("Second Job", world.player)
         )
+    if max_floor > 20:
+        for floor in range(2, max_floor // 10):
+            set_rule(
+                get_entrance(str(floor * 10 + 1) + "-" + str(floor * 10 + 10)),
+                lambda state: state.count_group("Equipment", world.player) + state.count_group("Magick", world.player) + state.count_group("Technick", world.player) + state.count_group("Mist", world.player) >= floor * 4
+            )
