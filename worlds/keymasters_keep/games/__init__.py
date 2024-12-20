dd5d2b36d2ee6b6faca7c49e6af3749698b7a08a
Nicholas Brochu <info@nicholasbrochu.com>
2024-12-20 13:34:58 -0500

    add game backlog metagame

--- worlds/keymasters_keep/games/__init__.py
+++ worlds/keymasters_keep/games/__init__.py
@@ -6,18 +6,21 @@ from ..game import Game
 
 from .street_fighter_6_game import StreetFighter6Game, StreetFighter6ArchipelagoOptions
 
+from .game_backlog_game import GameBacklogGame, GameBacklogArchipelagoOptions
+
 
 games: Dict[str, Type[Game]] = {
     StreetFighter6Game.game_name_with_platforms(): StreetFighter6Game,
 }
 
 metagames: Dict[str, Type[Game]] = {
-
+    GameBacklogGame.game_name_with_platforms(): GameBacklogGame,
 }
 
 
 @dataclass
 class GameArchipelagoOptions(
+    GameBacklogArchipelagoOptions,
     StreetFighter6ArchipelagoOptions
 ):
     pass
---
+++ worlds/keymasters_keep/games/game_backlog_game.py
@@ -0,0 +1,74 @@
+from typing import List
+
+from dataclasses import dataclass
+
+from Options import OptionSet
+
+from ..game import Game
+from ..game_objective_template import GameObjectiveTemplate
+
+from ..enums import KeymastersKeepGamePlatforms
+
+
+class GameBacklogGame(Game):
+    name = "Game Backlog"
+    platform = KeymastersKeepGamePlatforms.META
+
+    platforms_other = None
+
+    is_adult_only_or_unrated = False
+
+    def optional_game_constraint_templates(self) -> List[GameObjectiveTemplate]:
+        return list()
+
+    def game_objective_templates(self) -> List[GameObjectiveTemplate]:
+        return [
+            GameObjectiveTemplate(
+                label="Game Backlog: ACTION GAME",
+                data={"ACTION": (self.actions, 1), "GAME": (self.games, 1)},
+                is_time_consuming=True,
+                is_difficult=False,
+                weight=1,
+            ),
+        ]
+
+    def actions(self) -> List[str]:
+        return sorted(self.archipelago_options.game_backlog_actions.value)
+
+    def games(self) -> List[str]:
+        return sorted(self.archipelago_options.game_backlog_game_selection.value)
+
+
+# Archipelago Options
+class GameBacklogGameSelection(OptionSet):
+    """
+    Defines which games are in the player's backlog.
+
+    Replace the placeholders with values of your own choosing.
+    """
+
+    display_name = "Game Backlog Game Selection"
+
+    default = ["Game 1", "Game 2", "..."]
+
+
+class GameBacklogActions(OptionSet):
+    """
+    Defines the possible actions that could be required on a game in the backlog.
+    """
+
+    display_name = "Game Backlog Actions"
+
+    valid_keys = [
+        "TRY",
+        "FINISH",
+        "COMPLETE",
+    ]
+
+    default = valid_keys
+
+
+@dataclass
+class GameBacklogArchipelagoOptions:
+    game_backlog_game_selection: GameBacklogGameSelection
+    game_backlog_actions: GameBacklogActions
