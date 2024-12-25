a603924b6d7b5769ef8e4d55688515d5b1b65b51
Nicholas Brochu <info@nicholasbrochu.com>
2024-12-25 10:23:05 -0500

    add trombone champ

--- worlds/keymasters_keep/games/__init__.py
+++ worlds/keymasters_keep/games/__init__.py
@@ -12,6 +12,7 @@ from .archipelago_multiworld_randomizer_game import (
 from .pinball_fx3_game import PinballFX3Game, PinballFX3ArchipelagoOptions
 from .street_fighter_6_game import StreetFighter6Game, StreetFighter6ArchipelagoOptions
 from .trackmania_game import TrackmaniaGame, TrackmaniaArchipelagoOptions
+from .trombone_champ_game import TromboneChampGame, TromboneChampArchipelagoOptions
 
 # Metagame Imports
 from .game_backlog_game import GameBacklogGame, GameBacklogArchipelagoOptions
@@ -22,6 +23,7 @@ games: Dict[str, Type[Game]] = {
     PinballFX3Game.game_name_with_platforms(): PinballFX3Game,
     StreetFighter6Game.game_name_with_platforms(): StreetFighter6Game,
     TrackmaniaGame.game_name_with_platforms(): TrackmaniaGame,
+    TromboneChampGame.game_name_with_platforms(): TromboneChampGame,
 }
 
 metagames: Dict[str, Type[Game]] = {
@@ -32,6 +34,7 @@ metagames: Dict[str, Type[Game]] = {
 @dataclass
 class GameArchipelagoOptions(
     # Add in reverse alphabetical order
+    TromboneChampArchipelagoOptions,
     TrackmaniaArchipelagoOptions,
     StreetFighter6ArchipelagoOptions,
     PinballFX3ArchipelagoOptions,
