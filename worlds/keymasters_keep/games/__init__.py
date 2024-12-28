10f12aba3106063de8b678b6d765bae741718535
Nicholas Brochu <info@nicholasbrochu.com>
2024-12-28 13:59:12 -0500

    add ultrakill

--- worlds/keymasters_keep/games/__init__.py
+++ worlds/keymasters_keep/games/__init__.py
@@ -8,10 +8,6 @@ from ..game import Game
 from .a_dance_of_fire_and_ice_game import ADanceOfFireAndIceGame, ADanceOfFireAndIceArchipelagoOptions
 from .anger_foot_game import AngerFootGame, AngerFootArchipelagoOptions
 
-from .archipelago_multiworld_randomizer_game import (
-    ArchipelagoMultiworldRandomizerGame, ArchipelagoMultiworldRandomizerArchipelagoOptions
-)
-
 from .neon_white_game import NeonWhiteGame, NeonWhiteArchipelagoOptions
 from .pinball_fx3_game import PinballFX3Game, PinballFX3ArchipelagoOptions
 
@@ -26,8 +22,13 @@ from .star_wars_battlefront_ii_classic_game import (
 from .street_fighter_6_game import StreetFighter6Game, StreetFighter6ArchipelagoOptions
 from .trackmania_game import TrackmaniaGame, TrackmaniaArchipelagoOptions
 from .trombone_champ_game import TromboneChampGame, TromboneChampArchipelagoOptions
+from .ultrakill_game import UltrakillGame, UltrakillArchipelagoOptions
 
 # Metagame Imports
+from .archipelago_multiworld_randomizer_game import (
+    ArchipelagoMultiworldRandomizerGame, ArchipelagoMultiworldRandomizerArchipelagoOptions
+)
+
 from .game_backlog_game import GameBacklogGame, GameBacklogArchipelagoOptions
 from .retro_achievements_game import RetroAchievementsGame, RetroAchievementsArchipelagoOptions
 
@@ -42,6 +43,7 @@ games: Dict[str, Type[Game]] = {
     StreetFighter6Game.game_name_with_platforms(): StreetFighter6Game,
     TrackmaniaGame.game_name_with_platforms(): TrackmaniaGame,
     TromboneChampGame.game_name_with_platforms(): TromboneChampGame,
+    UltrakillGame.game_name_with_platforms(): UltrakillGame,
 }
 
 metagames: Dict[str, Type[Game]] = {
@@ -54,6 +56,7 @@ metagames: Dict[str, Type[Game]] = {
 @dataclass
 class GameArchipelagoOptions(
     # Add in reverse alphabetical order
+    UltrakillArchipelagoOptions,
     TromboneChampArchipelagoOptions,
     TrackmaniaArchipelagoOptions,
     StreetFighter6ArchipelagoOptions,
