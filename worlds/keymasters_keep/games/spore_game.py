006e6e9857948517fdc2f0c94ee2bfeb4287dedf
MarioSpore <45576468+MarioSpore@users.noreply.github.com>
2025-01-25 02:13:02 -0500

    KMK: Sporecast Missions update (#66)
    * Update spore_game.py
    
    Missing Sporecast adventures that were technically made by Maxis, even though they were actually made by the "Robot Chicken" writers. It's official since its under the Maxis account & gains 100 points upon completion.
    
    Though, there are Maxis dev made levels, but they're not official since its not under the official Maxis account & don't gain 100 points.
    
    * Mission renames and additions
    
    + Renamed "The Meaningless" to the "The Meaningless Turtle" to match ingame name
    + Added various more Maxis missions that I thought were not made by Maxis and were made by the writers of Robot Chicken. Still a Maxis adventure and you can earn 100 points so its technically a Maxis adventure.

--- worlds/keymasters_keep/games/spore_game.py
+++ worlds/keymasters_keep/games/spore_game.py
@@ -386,10 +386,21 @@ class SporeSporecastExclusiveMissions(OptionSet):
         "Robots vs Dragons",
         "Spoffit Calculator!",
         "SporeBall",
-        "The Meaningless",
+        "The Meaningless Turtle",
         "The Metamorphosis",
         "The Space Race!",
         "Welcome to Dancetopia",
+        "Bloody Sundae",
+        "Incontinent Continent",
+        "Litterbox Gulch",
+        "My Big Fat Pig Wedding",
+        "Planet of Needin'",
+        "Readerdome",
+        "Shake It Up",
+        "Shenanigans' Fun-Tasy Theme Park",
+        "That's My Lunch!",
+        "The Chicken and the Road",
+        "Whiney and his Poo",
     ]
 
     default = valid_keys
