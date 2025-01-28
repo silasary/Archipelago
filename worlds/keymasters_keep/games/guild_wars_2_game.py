5bb598eb64af1bca23afefbdc0932d32fa4aea0f
Feldar <pschwarz99@gmail.com>
2025-01-28 08:12:03 -0800

    Kk (#72)
    * Test change
    
    * Adjusted weights to make "Craft a Legendary" Task much rarer
    
    * Separated Jumping Puzzles, Fishing, and Crafting into their own categories
    
    * Reduced required amount for logging extras

--- worlds/keymasters_keep/games/guild_wars_2_game.py
+++ worlds/keymasters_keep/games/guild_wars_2_game.py
@@ -11,7 +11,6 @@ from ..game_objective_template import GameObjectiveTemplate
 
 from ..enums import KeymastersKeepGamePlatforms
 
-
 @dataclass
 class GuildWars2ArchipelagoOptions:
     guild_wars_2_storylines_owned: GuildWars2StorylinesOwned
@@ -56,7 +55,7 @@ class GuildWars2Game(Game):
                 },
                 is_time_consuming=False,
                 is_difficult=False,
-                weight=3,
+                weight=15,
             ),
             GameObjectiveTemplate(
                 label="Do DAILY_CATEGORY Dailies",
@@ -65,7 +64,7 @@ class GuildWars2Game(Game):
                 },
                 is_time_consuming=True,
                 is_difficult=False,
-                weight=len(self.daily_categories()),
+                weight=len(self.daily_categories()) * 5,
             ),
             GameObjectiveTemplate(
                 label="Do a WEEKLY_CATEGORY Weekly",
@@ -74,14 +73,14 @@ class GuildWars2Game(Game):
                 },
                 is_time_consuming=True,
                 is_difficult=False,
-                weight=len(self.weekly_categories()),
+                weight=len(self.weekly_categories()) * 5,
             ),
             GameObjectiveTemplate(
                 label="Do a Wizard's Vault Special",
                 data=dict(),
                 is_time_consuming=True,
                 is_difficult=False,
-                weight=2,
+                weight=10,
             ),
         ]
 
@@ -95,7 +94,7 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=True,
                     is_difficult=False,
-                    weight=2,
+                    weight=10,
                 ),
                 GameObjectiveTemplate(
                     label="Visit the following vistas: VISTAS",
@@ -104,7 +103,7 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=True,
                     is_difficult=False,
-                    weight=2,
+                    weight=10,
                 ),
                 GameObjectiveTemplate(
                     label="Visit all points of interest in ZONE",
@@ -113,7 +112,7 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=False,
                     is_difficult=False,
-                    weight=1,
+                    weight=5,
                 ),
                 GameObjectiveTemplate(
                     label="Unlock all waypoints in ZONE",
@@ -122,8 +121,12 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=False,
                     is_difficult=False,
-                    weight=1,
+                    weight=5,
                 ),
+            ]
+
+        if "Jumping Puzzle" in self.game_modes_played:
+            objective_list += [
                 GameObjectiveTemplate(
                     label="Complete the JUMPING_PUZZLE jumping puzzle",
                     data={
@@ -131,11 +134,23 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=False,
                     is_difficult=False,
-                    weight=1,
+                    weight=5,
                 ),
             ]
 
-        if "Gathering & Crafting" in self.game_modes_played:
+        if "End of Dragons" in self.storylines_owned and "Fishing" in self.game_modes_played:
+            objective_list.append(GameObjectiveTemplate(
+                label="Catch COUNTx FISHABLE",
+                data={
+                    "COUNT": (self.fishing_count_range, 1),
+                    "FISHABLE": (self.fishables, 1),
+                },
+                is_time_consuming=True,
+                is_difficult=False,
+                weight=5,
+            ))
+
+        if "Gathering" in self.game_modes_played:
             objective_list += [
                 GameObjectiveTemplate(
                     label="Harvest COUNTx HARVESTABLE",
@@ -145,7 +160,7 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=True,
                     is_difficult=False,
-                    weight=2,
+                    weight=10,
                 ),
                 GameObjectiveTemplate(
                     label="Log COUNTx LOGGABLE",
@@ -155,7 +170,7 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=True,
                     is_difficult=False,
-                    weight=1,
+                    weight=5,
                 ),
                 GameObjectiveTemplate(
                     label="Log COUNTx LOGGABLE",
@@ -165,7 +180,7 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=True,
                     is_difficult=False,
-                    weight=1,
+                    weight=5,
                 ),
                 GameObjectiveTemplate(
                     label="Mine COUNTx MINEABLE",
@@ -175,7 +190,7 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=True,
                     is_difficult=False,
-                    weight=1,
+                    weight=5,
                 ),
                 GameObjectiveTemplate(
                     label="Mine COUNTx MINEABLE",
@@ -185,22 +200,11 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=True,
                     is_difficult=False,
-                    weight=1,
+                    weight=5,
                 ),
             ]
 
-            if "End of Dragons" in self.storylines_owned:
-                objective_list.append(GameObjectiveTemplate(
-                    label="Catch COUNTx FISHABLE",
-                    data={
-                        "COUNT": (self.fishing_count_range, 1),
-                        "FISHABLE": (self.fishables, 1),
-                    },
-                    is_time_consuming=True,
-                    is_difficult=False,
-                    weight=1,
-                ))
-
+        if "Crafting" in self.game_modes_played:
             objective_list.append(
                 GameObjectiveTemplate(
                     label="Reach level LEVEL in DISCIPLINE",
@@ -210,7 +214,7 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=True,
                     is_difficult=False,
-                    weight=2,
+                    weight=10,
                 )
             )
 
@@ -222,7 +226,7 @@ class GuildWars2Game(Game):
                 },
                 is_time_consuming=True,
                 is_difficult=False,
-                weight=5,
+                weight=25,
             ))
 
         if "Story" in self.game_modes_played:
@@ -233,7 +237,7 @@ class GuildWars2Game(Game):
                 },
                 is_time_consuming=False,
                 is_difficult=False,
-                weight=3,
+                weight=15,
             ))
 
         if "WvW" in self.game_modes_played:
@@ -245,14 +249,14 @@ class GuildWars2Game(Game):
                     },
                     is_time_consuming=False,
                     is_difficult=False,
-                    weight=5,
+                    weight=25,
                 ),
                 GameObjectiveTemplate(
                     label="Earn a large skirmish chest",
                     data=dict(),
                     is_time_consuming=True,
                     is_difficult=False,
-                    weight=2,
+                    weight=10,
                 ),
             ]
 
@@ -264,7 +268,7 @@ class GuildWars2Game(Game):
                 },
                 is_time_consuming=False,
                 is_difficult=True,
-                weight=3,
+                weight=15,
             ))
 
         if "Fractals" in self.game_modes_played:
@@ -276,7 +280,7 @@ class GuildWars2Game(Game):
                 },
                 is_time_consuming=False,
                 is_difficult=True,
-                weight=3,
+                weight=15,
             ))
 
         if "Dungeons" in self.game_modes_played:
@@ -287,7 +291,7 @@ class GuildWars2Game(Game):
                 },
                 is_time_consuming=True,
                 is_difficult=False,
-                weight=3,
+                weight=15,
             ))
 
         if "Raids" in self.game_modes_played and len(self.raids()) > 0:
@@ -298,7 +302,7 @@ class GuildWars2Game(Game):
                 },
                 is_time_consuming=True,
                 is_difficult=False,
-                weight=2,
+                weight=10,
             ))
 
         if "Strikes" in self.game_modes_played:
@@ -309,7 +313,7 @@ class GuildWars2Game(Game):
                 },
                 is_time_consuming=True,
                 is_difficult=False,
-                weight=3,
+                weight=15,
             ))
 
         if "Convergences" in self.game_modes_played:
@@ -320,7 +324,7 @@ class GuildWars2Game(Game):
                 },
                 is_time_consuming=True,
                 is_difficult=False,
-                weight=3,
+                weight=15,
             ))
 
         return objective_list
@@ -385,7 +389,7 @@ class GuildWars2Game(Game):
             categories += ["Living World Season 4"]
         if "Season 3" in self.storylines_owned:
             categories += ["Living World Season 3"]
-        if "Janthir Wilds" in self.storylines_owned:
+        if "Janthir Wilds" in self.storylines_owned and "Jumping Puzzles" in self.game_modes_played:
             categories += ["Buzzy Treetops"]
 
         return categories
@@ -1213,7 +1217,7 @@ class GuildWars2Game(Game):
 
     @staticmethod
     def logging_count_range_low() -> range:
-        return range(10, 31)
+        return range(3, 10)
 
     @staticmethod
     def logging_count_range_high() -> range:
@@ -1905,8 +1909,11 @@ class GuildWars2GameModes(OptionSet):
 
     display_name = "Guild Wars 2 Game Modes"
     valid_keys = [
+        "Jumping Puzzles",
         "Exploration",
-        "Gathering & Crafting",
+        "Fishing",
+        "Gathering",
+        "Crafting",
         "Open World",
         "Story",
         "PvP",
