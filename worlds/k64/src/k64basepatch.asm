3a2e68633ddb2372789b10b5ec5c030bc4d5d969
Silvris <58583688+Silvris@users.noreply.github.com>
2025-04-05 18:12:31 -0500

    kivymd compat, fix friend crashes

--- worlds/k64/src/k64basepatch.asm
+++ worlds/k64/src/k64basepatch.asm
@@ -310,7 +310,7 @@ nop
 
 BridgeDededeOverride: //; t1 replace with 4f
 lui     t1, 0x800D
-lb      t1, 0x6C80 (t1)
+lb      t1, 0x6C82 (t1)
 bnez    t1, @@SetCorrect
 nop
 
@@ -320,7 +320,7 @@ nop
 @@SetCorrect:
 li      t1, 0x004F
 @@Return:
-sb      t1, 0x000C (v0)
+sb      t1, 0x000C (a0)
 j       0x8021F100
 nop
 
@@ -366,7 +366,7 @@ nop
 j       0x80228EE4
 nop
 @@Continue:
-sb      t9, 0x000C (v0)
+sb      t9, 0x000C (a0)
 j       0x80228EE4
 nop
 
@@ -378,9 +378,9 @@ nop
 j       0x8022857C
 nop
 @@Continue:
-sb      t4, 0x000C (v0)
+sb      t4, 0x000C (a2)
 jal     0x800B1900
-lhu     a0, 0x0002 (a2)
+lhu     a0, 0x0002 (v0)
 j       0x8022857C
 nop
 
@@ -395,6 +395,7 @@ nop
 @@SetCorrect:
 li      t9, 0x0050
 @@Return:
+sw      t9, 0x000C (v0)
 j       0x80222088
 nop
 
