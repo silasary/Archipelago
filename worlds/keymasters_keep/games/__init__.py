d4617e2287b0d372e7920955bc0d32ee2c13d25a
Nicholas Brochu <info@nicholasbrochu.com>
2025-04-23 08:34:35 -0400

    remove bundled games; add capability to load external games from a keymasters_keep directory

--- worlds/keymasters_keep/games/__init__.py
+++ worlds/keymasters_keep/games/__init__.py
@@ -1,15 +1,36 @@
 from typing import List, Type
 
-from dataclasses import dataclass
+import dataclasses
+import importlib.machinery
+import importlib.util
+import pathlib
+import pkgutil
+import sys
+import types
+
 from importlib import import_module
-from pkgutil import iter_modules
+
+from Utils import user_path
 
 from ..game import AutoGameRegister, Game
 
-for game_module_info in iter_modules(__path__):
+# Bundled games
+for game_module_info in pkgutil.iter_modules(__path__):
     import_module(f".{game_module_info.name}", __package__)
 
+# External games
+games_path: pathlib.Path = pathlib.Path(user_path("keymasters_keep"))
 
+game_path: pathlib.Path
+for game_path in games_path.glob("*.py"):
+    module_name: str = f"worlds.keymasters_keep.games.{game_path.stem}"
+    module_spec: importlib.machinery.ModuleSpec = importlib.util.spec_from_file_location(module_name, str(game_path))
+    module: types.ModuleType = importlib.util.module_from_spec(module_spec)
+
+    sys.modules[module_name] = module
+    module_spec.loader.exec_module(module)
+
+# Archipelago options
 option_classes: List[Type] = list()
 
 # Reverse order here is needed so that the options are added in alphabetical order in the YAML
@@ -18,6 +39,6 @@ for _, game_cls in sorted(AutoGameRegister.games.items(), reverse=True):
     option_classes.append(game_cls.options_cls)
 
 
-@dataclass
+@dataclasses.dataclass
 class GameArchipelagoOptions(*option_classes):
     pass
