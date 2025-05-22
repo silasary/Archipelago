57e68f2b0f2b9d143d9ba600ec8c6081f2a590fe
Benny Dreamly <bgaming0277@gmail.com>
2025-05-22 16:28:39 -0600

    add this to appease git gods

---
+++ package_apworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VERSION="1.6.0"
+
+# Ensure the versioned release directory exists
+mkdir -p "releases/${VERSION}"
+
+# Define exclude patterns
+EXCLUDES=(
+  "*/.DS_Store"
+  "*/__pycache__/*"
+  "*/__MACOSX/*"
+  "s4ap_0.1.11b_Messages.txt"
+  "items-testing-please-ignore-me.json"
+)
+
+# Create the zip file in the versioned release folder
+zip -r releases/${VERSION}/sims4.apworld worlds/sims4 -x "${EXCLUDES[@]}"
