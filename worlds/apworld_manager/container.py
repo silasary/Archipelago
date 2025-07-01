0c99766c6cf01088415007d0297c30411ab482bb
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-07-01 13:00:12 +1000

    Read archipelago.json files from within subfolders.  What could go wrong?

--- worlds/apworld_manager/container.py
+++ worlds/apworld_manager/container.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
+import json
 import zipfile
 
-from typing import (Dict, Any, TYPE_CHECKING)
+from typing import Dict, Any
 
-if TYPE_CHECKING:
-    from Utils import Version
+from Utils import tuplize_version, Version
 
 try:
     from worlds.Files import APWorldContainer
@@ -19,7 +19,10 @@ class RepoWorldContainer(APWorldContainer):
     description: str | None = None
 
     def read_contents(self, opened_zipfile: zipfile.ZipFile) -> Dict[str, Any]:
-        manifest = super().read_contents(opened_zipfile)
+        try:
+            manifest = super().read_contents(opened_zipfile)
+        except KeyError:
+            manifest = self.read_incorrect_contents(opened_zipfile)
         for string_key in ("github", "author", "description", "world_version_full"):
             if string_key in manifest:
                 setattr(self, string_key, manifest[string_key])
@@ -31,4 +34,22 @@ class RepoWorldContainer(APWorldContainer):
             string = getattr(self, string_key, None)
             if string:
                 manifest[string_key] = string
+        return manifest
+
+    def read_incorrect_contents(self, opened_zipfile: zipfile.ZipFile) -> Dict[str, Any]:
+        """Read contents of the zipfile from an archipelago.json file in the subdirectory."""
+        manifest = {}
+        for file in opened_zipfile.namelist():
+            if file.endswith("archipelago.json"):
+                with opened_zipfile.open(file) as f:
+                    contents = f.read()
+                manifest = json.loads(contents)
+        for string_key in ("game",):
+            if string_key in manifest:
+                setattr(self, string_key, manifest[string_key])
+
+        for version_key in ("world_version", "minimum_ap_version", "maximum_ap_version"):
+            if version_key in manifest:
+                setattr(self, version_key, Version(*tuplize_version(manifest[version_key])))
+
         return manifest
