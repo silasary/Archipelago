8a8c85a86de30cefdf268e54ddf67ecba7539011
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-11-18 23:18:22 +1100

    Only assign settings if they exist

--- worlds/factorio_saws/data/mod/settings-final-fixes.lua
+++ worlds/factorio_saws/data/mod/settings-final-fixes.lua
@@ -1,10 +1,18 @@
 -- Force SAWS settings because the mod generation assume a specific config
-data.raw["string-setting"]["saws-scrap-recipe"].hidden = true
-data.raw["string-setting"]["saws-scrap-recipe"].allowed_values = {"space-age"}
-data.raw["string-setting"]["saws-scrap-recipe"].default_value = "space-age"
-data.raw["bool-setting"]["saws-lava"].hidden = true
-data.raw["bool-setting"]["saws-lava"].forced_value = true
-data.raw["bool-setting"]["saws-lava"].default_value = true
-data.raw["bool-setting"]["saws-bacteria-cultivation"].hidden = true
-data.raw["bool-setting"]["saws-bacteria-cultivation"].forced_value = true
-data.raw["bool-setting"]["saws-bacteria-cultivation"].default_value = true
+if data.raw["string-setting"]["saws-scrap-recipe"] then
+    data.raw["string-setting"]["saws-scrap-recipe"].hidden = true
+    data.raw["string-setting"]["saws-scrap-recipe"].allowed_values = { "space-age" }
+    data.raw["string-setting"]["saws-scrap-recipe"].default_value = "space-age"
+end
+
+if data.raw["bool-setting"]["saws-lava"] then
+    data.raw["bool-setting"]["saws-lava"].hidden = true
+    data.raw["bool-setting"]["saws-lava"].forced_value = true
+    data.raw["bool-setting"]["saws-lava"].default_value = true
+end
+
+if data.raw["bool-setting"]["saws-bacteria-cultivation"] then
+    data.raw["bool-setting"]["saws-bacteria-cultivation"].hidden = true
+    data.raw["bool-setting"]["saws-bacteria-cultivation"].forced_value = true
+    data.raw["bool-setting"]["saws-bacteria-cultivation"].default_value = true
+end
