725407a6eef5557374ae86313d866963ea1fb83e
Katelyn Gigante <clockwork.singularity@gmail.com>
2026-01-14 12:13:38 +1100

    Log when someone is overriding a bundled game

--- worlds/keymasters_keep/games/__init__.py
+++ worlds/keymasters_keep/games/__init__.py
@@ -1,33 +1,31 @@
-from typing import List, Type
-
-import dataclasses
 import importlib.machinery
 import importlib.util
+import logging
 import pathlib
 import pkgutil
 import sys
 import types
-
 from importlib import import_module
-import typing
 
-from Options import OptionGroup
 from Utils import user_path
 
-from ..game import AutoGameRegister, Game
+bundled_names: list[str] = []
 
 # Bundled games
 for game_module_info in pkgutil.iter_modules(__path__):
     import_module(f".{game_module_info.name}", __package__)
+    bundled_names.append(game_module_info.name)
 
 # External games
 games_path: pathlib.Path = pathlib.Path(user_path("keymasters_keep"))
 
-broken_games: List[str] = list()
+broken_games: list[str] = []
 broken_games_path: pathlib.Path = games_path / "_broken_games.txt"
 
 game_path: pathlib.Path
 for game_path in games_path.glob("*.py"):
+    if game_path.stem in bundled_names:
+        logging.warning(f"Overriding bundled Keymaster's Keep game: {game_path.stem} with external version.")
     module_name: str = f"worlds.keymasters_keep.games.{game_path.stem}"
     module_spec: importlib.machinery.ModuleSpec = importlib.util.spec_from_file_location(module_name, str(game_path))
     module: types.ModuleType = importlib.util.module_from_spec(module_spec)
